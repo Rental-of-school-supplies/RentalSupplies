47bdfba748017b4c038d30c63f9f0b861daff2fc
JinYoungLee <brenda524@naver.com>
2022-12-08 12:00:40 +0900

    feat: row별 수정, 삭제 버튼 추가

--- layout/product_manage_M.php
+++ layout/product_manage_M.php
@@ -12,11 +12,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script src="../jquery-fadethis-master/libs/jquery/jquery.js"></script>
     <script src="../jquery-fadethis-master/dist/jquery.fadethis.min.js"></script>
-
 </head>
 
 <body>
@@ -54,6 +54,8 @@
                         <th>물품명</th>
                         <th>전체 개수</th>
                         <th>남은 수량</th>
+                        <th>수정</th>
+                        <th>삭제</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -62,12 +64,16 @@
                         <td>보조 배터리</td>
                         <td>3</td>
                         <td>3</td>
+                        <td><a href="" class="btn btn-info">EDIT </a></td>
+                        <td><a href="" class="btn btn-danger">DELETE</a></td>
                     </tr>
                     <tr>
-                    <td><input type="hidden" id="1" name="id" value="1"></td>
+                    <td><input type="hidden" id="2" name="id" value="2"></td>
                         <td>담요</td>
                         <td>10</td>
                         <td>6</td>
+                        <td><a href="" class="btn btn-info">EDIT </a></td>
+                        <td><a href="" class="btn btn-danger">DELETE</a></td>
                     </tr>
                 </tbody>
             </table>
@@ -102,3 +108,7 @@
         </div>
     </footer>
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.4/js/tether.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
+</html>
